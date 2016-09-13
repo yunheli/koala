192a934489f024dc24510f8defc80f8cb95ad14b
Will <yh@fir.im>
2016-09-13 19:26:21 +0800

    添加original_name

--- app/uploaders/document_uploader.rb
+++ app/uploaders/document_uploader.rb
@@ -4,4 +4,8 @@ class DocumentUploader < CarrierWave::Uploader::Base
   def store_dir
     "#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
+
+  def filename
+    original_filename
+  end
 end
