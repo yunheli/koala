0ece6e7687ebd98c3659a29018049a88243bb647
Will <yh@fir.im>
2016-09-26 22:39:32 +0800

    添加bucket 的更新

--- app/controllers/buckets_controller.rb
+++ app/controllers/buckets_controller.rb
@@ -1,21 +1,29 @@
 class BucketsController < ApplicationController
 
+  before_action :set_bucket
   def create
     bucket = Bucket.create(param_permit)
     render json: bucket
   end
 
   def show
+    render json: @bucket
   end
 
   def index
   end
 
   def update
+    @bucket.update(param_permit)
+    render :show
   end
 
   private
   def param_permit
     params.permit(:name, :is_private)
+  end
+
+  def set_bucket
+    @bucket = Bucket.find(params[:id])
   end
 end
